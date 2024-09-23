36217d3e3a30cb0e03b4e215b0e009b0cb7d42f0
Diego2312 <diego.escoala2004@gmail.com>
2024-09-23 19:02:52 +0200

    pandas 1 basic df

--- pandas1.py
+++ pandas1.py
@@ -6,10 +6,12 @@ import pandas as pd
 df = pd.DataFrame([[1, 2, 3], [4,3,5], [6, 7, 8], [9, 10, 11], [12, 13, 14], [15, 16, 17]], columns=["A", 'B', "C"], index=["x", "y", "z", "k", "l", "m"]) #Create simple data frame out of python lists
 
 """
-
-print(df.head()) #Vizualize first 5 rows of data frame
-print(df.columns) #Vizualize the columns in the df
-print(df.index.tolist()) #Vizualize the indexes (rows) in the df
+print(df) #Vizualize full dataframe
+print(df.head()) #Vizualize first 5 rows of data frame (can pass in first number of rows desired to see)
+print(df.tail()) #Same as head but last rows
+print(df.sample()) #a sample of data (random)
+print(df.columns) #the columns in the df
+print(df.index.tolist()) #the indexes (rows) in the df
 print(df.info) #Vizualize info on the df (bytes used, size)
 print(df.describe) #Useful information (count, mean, standard deviation)
 print(df.size) #columns and rows
@@ -19,8 +21,14 @@ print(df.size) #columns and rows
 
 
 coffee = pd.read_csv(r"C:\Users\Owner\ACSAI\Extra\pandas-study\data\coffee.csv") #Create a df by loading a csv (comma separated values) file. ALso possible with other formats
+results_parquet = pd.read_parquet(r"C:\Users\Owner\ACSAI\Extra\pandas-study\data\results.parquet") #Loading a parquet file
+bios = pd.read_csv(r"https://raw.githubusercontent.com/KeithGalli/complete-pandas-tutorial/refs/heads/master/data/bios.csv") #loading a file from github. Not locally downloaded
 
-results_parquet = pd.read_parquet(r"C:\Users\Owner\ACSAI\Extra\pandas-study\data\results.parquet")
+#print(coffee)
 
-print(coffee.head())
-print(results_parquet.head())
+
+
+print(coffee.loc[[3, 10], ["Coffee Type"]]) #access data by specifying rows and columns (can use indexes and strings) [[rows], [columns]]
+print(coffee.iloc[4:10, [2]]) #difference is columns are specified by index instead of name (in ilox upper index is not included)
+
+coffee.loc[5, "Units sold"] = 1203 #Modify a value in the df using loc
